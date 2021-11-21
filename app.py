6b5a72ebdf3804de5a5ad3f89b6a2965ef29ae39
892nske <tomo.kadokura@gmail.com>
2021-11-22 00:27:16 +0900

    plot

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import dcc
 from dash import html 
 import plotly.graph_objects as go
 import pandas as pd
+import psycopg2
 
 
 app = dash.Dash()
@@ -10,18 +11,34 @@ app = dash.Dash()
 server = app.server
 
 
-data_spot = pd.read_csv('BTCUSD_spot.csv')
-data_perp = pd.read_csv('BTCUSD_perp.csv')
+# 接続情報
+connection_config = {
+    'host': 'rdsnske.c7sczatjnphy.ap-northeast-1.rds.amazonaws.com',
+    'port': '5432',
+    'database': 'bot',
+    'user': 'rdsuser',
+    'password': 'rds%5678'
+}
+
+# 接続
+connection = psycopg2.connect(**connection_config)
+
+# DataFrameでロード
+data_spot = pd.read_sql(sql="SELECT * FROM bybit_api_spot where symbol = 'BTCUSDT';", con=connection )
+data_perp = pd.read_sql(sql="SELECT * FROM bybit_api_futures where symbol = 'BTCUSDT';", con=connection )
+
+# data_spot = pd.read_csv('BTCUSD_spot.csv')
+# data_perp = pd.read_csv('BTCUSD_perp.csv')
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data_spot['timestamp'],
-                          y=data_spot['close'],
+fig.add_trace(go.Scatter(x=data_spot['basetime'],
+                          y=data_spot['last'],
                           mode='lines',#plotの種類
                           name='spot' #plotの名前
                           ))
                           
-fig.add_trace(go.Scatter(x=data_perp['timestamp'],
-                          y=data_perp['close'],
+fig.add_trace(go.Scatter(x=data_perp['basetime'],
+                          y=data_perp['last_price'],
                           mode='lines',#plotの種類
                           name='perp' #plotの名前
                           ))
